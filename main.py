6fe926417d23ba953b827b3f52b87787cac97976
Paweł Wasil <pawel.wasil@britenet.com.pl>
2019-08-28 22:38:25 +0200

    healing player

--- main.py
+++ main.py
@@ -31,21 +31,28 @@ while running:
         dmg = player.generate_damage()
         enemy.take_damage(int(dmg))
         print('You attack for ', dmg, ' points of damage.')
+
     elif index == 1:
         player.choose_magic()
         magic_choice = int(input('Choice magic: ')) - 1
-
         spell = player.magic[magic_choice]
         magic_dmg = spell.generate_damage()
-
         current_mp = player.get_mp()
+
         if spell.cost > current_mp:
             print(BColors.FAIL + "\nNot enough MP \n" + BColors.END_C)
             continue
+
         player.reduce_mp(spell.cost)
-        enemy.take_damage(magic_dmg)
-        print(BColors.OK_BLUE + " \n" + spell.name + " deals ", str(magic_dmg), "damage points of magic.\n",
-              BColors.END_C)
+
+        if spell.type == 'white':
+            player.heal(magic_dmg)
+            print(BColors.OK_BLUE + "\n" + spell.name + " health for " + str(magic_dmg) + ' HP.' + BColors.END_C)
+
+        elif spell.type == 'black':
+            enemy.take_damage(magic_dmg)
+            print(BColors.OK_BLUE + " \n" + spell.name + " deals ", str(magic_dmg), "damage points of magic.\n",
+                  BColors.END_C)
 
     enemy_choice = 1
     enemy_dmg = enemy.generate_damage()
@@ -61,6 +68,7 @@ while running:
     if enemy.get_hp() == 0:
         print(BColors.OK_GREEN + 'You win!' + BColors.END_C)
         running = False
+
     elif player.get_hp() == 0:
         print(BColors.FAIL + 'You enemy has defeat you!' + BColors.END_C)
         running = False
